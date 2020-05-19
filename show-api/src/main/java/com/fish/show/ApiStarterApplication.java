0c652b88d0a5b3000492627a0d5901172b95467b
fish <lovefish360@163.com>
2020-05-20 00:01:02 +0800

    集成rabbitmq

---
+++ show-api/src/main/java/com/fish/show/ApiStarterApplication.java
@@ -0,0 +1,29 @@
+package com.fish.show;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * 启动程序
+ * @author
+ */
+
+@SpringBootApplication(scanBasePackages = "com.fish.*")
+@ServletComponentScan
+@EnableAsync
+@MapperScan(basePackages = "com.fish.show.mappers")
+public class ApiStarterApplication extends SpringBootServletInitializer
+{
+    public static void main(String[] args)
+    { SpringApplication.run(ApiStarterApplication.class, args); }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ApiStarterApplication.class);
+    }
+}
---
+++ show-rabbitmq-consumer/src/main/java/com/fish/show/ConsumerStarterApplication.java
@@ -0,0 +1,29 @@
+package com.fish.show;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * 启动程序
+ * @author
+ */
+
+@SpringBootApplication(scanBasePackages = "com.fish.*")
+@ServletComponentScan
+@EnableAsync
+@MapperScan(basePackages = "com.fish.show.mappers")
+public class ConsumerStarterApplication extends SpringBootServletInitializer
+{
+    public static void main(String[] args)
+    { SpringApplication.run(ConsumerStarterApplication.class, args); }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ConsumerStarterApplication.class);
+    }
+}
